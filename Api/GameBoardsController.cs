88f2ae7af1e062086de0aab5cf573945d3606826
Vadim Kandorov <Vadix3@gmail.com>
2021-08-18 02:40:48 +0300

    Added queries and about

--- Api/GameBoardsController.cs
+++ Api/GameBoardsController.cs
@@ -93,6 +93,8 @@ namespace tttServer.Api
             return gameBoard;
         }
 
+
+
         // PUT: api/GameBoards/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
--- Pages/Queries.cshtml.cs
+++ Pages/Queries.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using tttServer.Models;
 
 namespace tttServer.Pages
 {
     public class QueriesModel : PageModel
     {
+
+
+        private readonly Data.tttGamesContext _context;
+
+        public QueriesModel(Data.tttGamesContext context)
+        {
+            _context = context;
+        }
+
+
+
         public void OnGet()
         {
+
+            Console.WriteLine("Queries");
+
+
         }
+
+
     }
 }
